00e570bbd5385b0facdff0feb349fc31aca3f9f1
Johann-S <johann.servoire@gmail.com>
2015-04-10 17:48:23 +0200

    Finish experience page

--- src/Site/MainBundle/Resources/views/Default/experience.html.php
+++ src/Site/MainBundle/Resources/views/Default/experience.html.php
@@ -5,10 +5,32 @@
 <div class="panel panel-default">
     <div class="panel-heading">Experiences</div>
     <div class="panel-body">
+        <ul class="list-unstyled listExp">
+            <li>
+                <span class="label label-info expLabel">2012 <i class="fa fa-arrow-right"></i> 2015</span>Développeur Web pour la société <a href="http://www.interactiv-technologies.fr/">Interactiv' Technologies</a>
+            </li>
+            <li>
+                <span class="label label-info expLabel">2015</span>Arrivé troisième de la final national de <a href="https://www.imaginecup.com/">l'ImagineCup</a> catégorie innovation : <a href="http://www.omnitag.fr">http://www.omnitag.fr</a>
+            </li>
+            <li>
+                <span class="label label-info expLabel">2011</span>Stage de Développement web (6 semaines) pour l'entreprise <a href="http://www.alchimiste.fr/">Alchimiste</a>
+            </li>
+        </ul>
     </div>
 </div>
 <div class="panel panel-default">
     <div class="panel-heading">Etudes</div>
     <div class="panel-body">
+        <ul class="list-unstyled listExp">
+            <li>
+                <span class="label label-info expLabel">2015</span>Etudiant en quatrième année du cycle d'ingénierie à <a href="http://www.epsi.fr/">l'EPSI Lyon</a>.
+            </li>
+            <li>
+                <span class="label label-info expLabel">2011 <i class="fa fa-arrow-right"></i> 2012</span>BTS I.R.I.S (Informatique et Réseaux pour l'industrie et les Services techniques) - Lycée Edouard Branly - Lyon (69005)
+            </li>
+            <li>
+                <span class="label label-info expLabel">2009 <i class="fa fa-arrow-right"></i> 2010</span>Bac STI Electronique - Lycée Jean-Baptiste De La Salle - Lyon (69001)
+            </li>
+        </ul>
     </div>
 </div>
