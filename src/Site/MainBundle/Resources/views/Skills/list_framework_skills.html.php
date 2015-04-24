f5c23780a3475b08e9b2897ee49b8e0e13a9aaf7
Johann-S <johann.servoire@gmail.com>
2015-04-24 12:26:42 +0200

    Fix responsive design on skills page

--- src/Site/MainBundle/Resources/views/Skills/list_framework_skills.html.php
+++ src/Site/MainBundle/Resources/views/Skills/list_framework_skills.html.php
@@ -1,84 +1,100 @@
-<ul class="list-unstyled col-md-4 list_skills">
-    <li>
-        Symfony2
-        <span class="rating" style="margin-left: 60px;">
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star-half-empty"></span>
-        </span>
-    </li>
-    <li>
-        Zend2
-        <span class="rating" style="margin-left: 85px;">
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star-half-empty"></span>
-        </span>
-    </li>
-    <li>
-        Silex
-        <span class="rating" style="margin-left: 100px;">
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star-half-empty"></span>
-        </span>
-    </li>
-    <li>
-        Qt
-        <span class="rating" style="margin-left: 114px;">
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star-o"></span>
-        </span>
-    </li>
-</ul>
-<ul class="list-unstyled col-md-4 list_skills">
-    <li>
-        JQuery
-        <span class="rating" style="margin-left: 170px;">
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-        </span>
-    </li>
-    <li>
-        Bootstrap (Contributeur)
-        <span class="rating" style="margin-left: 43px;">
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-        </span>
-    </li>
-    <li>
-        Ratchet (Contributeur)
-        <span class="rating" style="margin-left: 60px;">
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-        </span>
-    </li>
-    <li>
-        QUnit
-        <span class="rating" style="margin-left: 182px;">
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star-o"></span>
-        </span>
-    </li>
-</ul>
+<div class="form-horizontal col-sm-5">
+    <div class="form-group">
+        <label class="col-sm-5 control-label" style="text-align: left;">Symfony2</label>
+        <div class="col-sm-4 col-rating">
+            <span class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-empty"></span>
+            </span>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-5 control-label" style="text-align: left;">Zend2</label>
+        <div class="col-sm-4 col-rating">
+            <span class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-empty"></span>
+            </span>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-5 control-label" style="text-align: left;">Silex</label>
+        <div class="col-sm-4 col-rating">
+            <span class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-empty"></span>
+            </span>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-5 control-label" style="text-align: left;">Qt</label>
+        <div class="col-sm-4 col-rating">
+            <span class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-o"></span>
+            </span>
+        </div>
+    </div>
+</div>
+<div class="form-horizontal col-sm-5">
+    <div class="form-group">
+        <label class="col-sm-6 control-label" style="text-align: left;">JQuery</label>
+        <div class="col-sm-4 col-rating">
+            <span class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+            </span>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-6 control-label" style="text-align: left;">Bootstrap (Contributeur)</label>
+        <div class="col-sm-4 col-rating">
+            <span class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+            </span>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-6 control-label" style="text-align: left;">Ratchet (Contributeur)</label>
+        <div class="col-sm-4 col-rating">
+            <span class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+            </span>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-6 control-label" style="text-align: left;">QUnit</label>
+        <div class="col-sm-4 col-rating">
+            <span class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-o"></span>
+            </span>
+        </div>
+    </div>
+</div>
